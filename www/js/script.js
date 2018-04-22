fe681521e7688a43b6fdc67fb45aea74d238c09d
lukasz24 <lukasz.pudzisz@gmail.com>
2018-04-22 20:22:43 +0200

    Change in Google Log-in

--- www/js/script.js
+++ www/js/script.js
@@ -1,16 +1,22 @@
+/*
+function goToSite(id){
+  
+  //window.location.href+='#' + id;
+  var anch = document.createElement('a');
+
+  anch.setAttribute("href", "#"+id);
+  anch.click();
+}
 
 var googleSignin = function() {
   var provider = new firebase.auth.GoogleAuthProvider();
-  //firebase.auth().currentUser.linkWithRedirect();
-   firebase.auth().signInWithRedirect(provider);
-   firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
+   firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+  // ..
+  console.log(user);
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
@@ -20,15 +26,17 @@ var googleSignin = function() {
   // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
   // ...
+
 });
 }
 
 var googleSignout = function() {
    firebase.auth().signOut()
-    
+	
    .then(function() {
       console.log('Signout Succesfull')
    }, function(error) {
       console.log('Signout Failed')  
    });
 }
+*/
