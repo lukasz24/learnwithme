6b9330a6efef07a7a3b79efc27c39dc3bdeb9f0b
lukasz24 <lukasz.pudzisz@gmail.com>
2018-04-23 12:56:19 +0200

    nothing working

--- www/js/script.js
+++ www/js/script.js
@@ -40,3 +40,47 @@ var googleSignout = function() {
    });
 }
 */
+(function() {
+	// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2E9hI9X2e2KCHyEiaz5vcaFXOFvY0HQc",
+    authDomain: "learnwithme-98129.firebaseapp.com",
+    databaseURL: "https://learnwithme-98129.firebaseio.com",
+    projectId: "learnwithme-98129",
+    storageBucket: "learnwithme-98129.appspot.com",
+    messagingSenderId: "1032899670271"
+  };
+  firebase.initializeApp(config);
+
+  
+
+}());
+$(document).ready(function(){
+  const emailInput = document.getElementById('email');
+  const passInput = document.getElementById('haslo');
+  const btnLogIn = document.getElementById('zaloguj');
+  const btnGLogIn = document.getElementById("zalogujG");
+  const btnFBLogIn = document.getElementById("zalogujFB");
+
+  btnLogIn.addEventListener('click', function() {
+  	//Pobranie email i hasła
+  	const email = emailInput.value;
+  	const passwd = passInput.value;
+  	const auth = firebase.auth();
+  	//Logowanie
+  	const promise = auth.signInWithEmailAndPassword(email, passwd);
+  	promise.catch(e => {console.log(e.message);
+  		var info = document.getElementById('info');
+  		info.innerText = "Niepoprawny email lub hasło!";
+  		info.style.color = "red";
+  		emailInput.style.backgroundColor = "#ff7777";
+  		passInput.style.backgroundColor = "#ff7777";
+  		
+  	});
+  });
+});
+/*
+var someDiv = document.getElementById('someDiv');
+  var dbRef = firebase.database().ref().child('text');
+  dbRef.on('value', snap => someDiv.innerText = snap.val());
+*/
