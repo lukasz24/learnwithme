db418a16d094002c096ebcfcf91830fc7599a404
lukasz24 <lukasz.pudzisz@gmail.com>
2018-04-23 09:44:04 +0200

    Try fix google login on phonegap

--- www/js/google.js
+++ www/js/google.js
@@ -1,8 +1,56 @@
 
 var googleSignin = function() {
   var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithRedirect(provider).then(function() {
+  return firebase.auth().getRedirectResult();
+}).then(function(result) {
+  // This gives you a Google Access Token.
+  // You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    // You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
   //firebase.auth().currentUser.linkWithRedirect();
-   firebase.auth().signInWithRedirect(provider);
+
+   /*
+   if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithRedirect(provider);
+        // [END signin]
+      } else {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+      }
+      */
+   /*
    firebase.auth().getRedirectResult().then(function(result) {
   if (result.credential) {
     // This gives you a Google Access Token. You can use it to access the Google API.
@@ -21,6 +69,7 @@ var googleSignin = function() {
   var credential = error.credential;
   // ...
 });
+*/
 }
 
 var googleSignout = function() {
