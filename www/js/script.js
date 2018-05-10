dea712f5b364d3c9455b2493402018e30e7ccdd2
Radosław Smyksy <rsmyksy@gmail.com>
2018-05-10 19:22:54 +0200

    bug changes

--- www/js/script.js
+++ www/js/script.js
@@ -232,7 +232,5 @@ function chartClick(){
 
 function sendMail()
 {
-  navigator.notification.alert(
-    'Wszelkie problemy i pytania proszę zgłaszać na adres e-mail: pomoc@pomoc.com'
-    );
+  alert('Wszelkie problemy i pytania proszę zgłaszać na adres e-mail: pomoc@pomoc.com');
 }
